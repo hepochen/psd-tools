28f002b3a7f47a8f5bf7a5ff0694c70c61724d04
Hepo <hepochen@gmail.com>
2015-11-17 21:38:53 +0800

    for py2exe

--- src/psd_tools/icc_profiles.py
+++ src/psd_tools/icc_profiles.py
@@ -4,6 +4,9 @@ import os
 
 GRAY_PATH = os.path.join(os.path.dirname(__file__), 'icc_profiles', 'Gray-CIE_L.icc')
 
+if not os.path.isfile(GRAY_PATH) and 'ICC_GRAY_PATH' in os.environ:
+    GRAY_PATH = os.environ.get('ICC_GRAY_PATH')
+
 try:
     from PIL import ImageCms
     gray = ImageCms.ImageCmsProfile(GRAY_PATH)
